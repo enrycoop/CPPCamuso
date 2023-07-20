a41e5205e83d3b993ef46d6ee9077170b7a0755a
Enrico Coppolecchia <48773922+enrycoop@users.noreply.github.com>
2023-07-20 12:29:21 +0200

    start

---
+++ ex1.0/ex1.0.cpp
@@ -0,0 +1,9 @@
+// ex1.0.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
