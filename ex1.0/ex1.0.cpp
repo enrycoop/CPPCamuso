73732e30f3bdce43e1e79363b40d05ddf452ec0e
Enrico Coppolecchia <48773922+enrycoop@users.noreply.github.com>
2023-07-20 18:16:21 +0200

    Fino a lezione 14

--- ex1.0/ex1.0.cpp
+++ ex1.0/ex1.0.cpp
@@ -1,9 +1,29 @@
 // ex1.0.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
-//
-
+// preprocessore include delle librerie
 #include <iostream>
 
+// preprocessore macro con parametri
+#define GETMAX(a,b) a>b?a:b
+
+
+int massimo(int x, int y) {
+    return GETMAX(x, y);
+}
+
+// main funzione principale
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Inserisci la somma in euro: ";
+    int euro;
+    std::cin >> euro;
+
+    std::cout << "Fattire di cambio? ";
+    double cambio;
+    std::cin >> cambio;
+    std::cout << "Cambio letto: " << cambio << std::endl;
+
+    std::cout << "In dollari: " 
+              << euro * cambio << std::endl;
+
+    
 }
