8644a149d304af24af7703984aeab202b579420d
Sergio Enrico Coppolecchia <enrico@localhost.localdomain>
2023-08-09 11:35:22 +0200

    espressioni

--- espressioni/precedenze/main.cpp
+++ espressioni/precedenze/main.cpp
@@ -3,24 +3,60 @@
 
 using namespace std;
 
+int f1(int &i){
+    i++;
+    return i;
+}
+
+int f2(int &i){
+    i+=100;
+    return i;
+}
+
 int main() {
-    int a=5, b=6, c=7, d=0;
-    d = a + b * c; // precedenza sul *
-    cout << d << endl;
+#if 0
+    {
+        int a = 5, b = 6, c = 7, d = 0;
+        d = a + b * c; // precedenza sul *
+        cout << d << endl;
 
-    d = a+(++b); // a++ + b
-    cout << d <<endl;
-    d = (a++)+(++b);
-    cout << d <<endl;
-    cout << a << " " << b << endl;
+        d = a + (++b); // a++ + b
+        cout << d << endl;
+        d = (a++) + (++b);
+        cout << d << endl;
+        cout << a << " " << b << endl;
 
 
-    bool test1 = false;
-    if(test1!=a>b)  // priorità al >
-        cout << "verificato\n";
+        bool test1 = false;
+        if (test1 != a > b)  // priorità al >
+            cout << "verificato\n";
 
-    a = 10;
-    d = a+=2*10;
-    cout << d << endl;
+        a = 10;
+        d = a += 2 * 10;
+        cout << d << endl;
+    }
+#endif
+    /*
+     * gli operatori che hanno la stessa priorità hanno la stessa associatività.
+     * esempi di associatività da sinistra a destra
+     */
+    cout << 5.0 + 6 * 3 << endl; // precedenza al *
+    // in assenza di parentesi la precedenza è in base a sinistra verso destra o viceversa a seconda dell'operatore
+    cout << 5.0 / 6 * 3 << endl; //l'ordine è da sinistra a destra
+    cout << (5.0 / 6) * 3 << endl;
+    cout << 5.0 / (6 * 3) << endl;
+
+    {
+        int i=30;
+        int j = i = 4; //associatività da destra verso sinistra
+        cout << "j: " << j << endl;
+    }
+
+    {
+        int i = 0, j = 100;
+        int z = f1(i) + f2(i); // a+b = b+a ??? risultato indefinito
+        cout << "z: " << z << endl;
+    }
+
     return 0;
 }
