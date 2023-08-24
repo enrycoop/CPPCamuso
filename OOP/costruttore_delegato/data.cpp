8a409f42e3a82265bdb490598df826006decb133
Enrico Coppolecchia <48773922+enrycoop@users.noreply.github.com>
2023-08-25 00:32:41 +0200

    OOP

---
+++ OOP/costruttore_delegato/data.cpp
@@ -0,0 +1,78 @@
+#include "data.h"
+
+//le variabili static non possono essere inizializzate nella classe
+//ma solo qui. Queste variabili vengono inizializzate solo nel momento
+//in cui viene avviato il programma e non viene modificato
+tm* Data::oggi = Data::data_corrente();
+
+tm* Data::data_corrente(){
+    time_t tempo_secondi = time(nullptr);
+    return localtime(&tempo_secondi);
+}
+
+Data::Data() {}
+
+bool Data::valida(int gg, int mm, int aa){
+    return gg>=1 && gg <=31 && mm>=1 && mm<=31 && aa>=1970;
+}
+
+//costruttore delegato
+Data::Data(int gg, int mm, int aa)
+{
+     if(valida(gg,mm,aa)){
+        giorno = gg;
+        mese = mm;
+        anno = aa;
+    } else { //impostiamo la data corrente
+        giorno = oggi->tm_mday;
+        mese = oggi->tm_mon;
+        anno = oggi->tm_year+1900;
+    }
+}
+
+Data::Data(int gg, int mm) : Data(gg, mm, data_corrente()->tm_year+1900)
+{ }
+
+Data::Data(int gg) : Data(gg, data_corrente()->tm_mon, data_corrente()->tm_year+1900)
+{ }
+
+Data::Data(const std::string d) : Data(stoi( d.substr(0,2) ),
+                                       stoi( d.substr(3,2) ),
+                                       stoi( d.substr(6,4) ))
+{ }
+/*
+Data::Data(int gg, int mm)
+{
+    tm* oggi = data_corrente();
+    int aa = oggi->tm_year+1900;
+
+    initialization(gg,mm,aa);
+}
+
+Data::Data(int gg)
+{
+    tm* oggi = data_corrente();
+    int aa = oggi->tm_year+1900;
+    int mm = oggi->tm_mon;
+
+    initialization(gg,mm,aa);
+}
+
+//per semplicita' imponiamo il formato fisso gg-mm-aaaa
+Data::Data(const std::string d)
+{
+    int gg = stoi( d.substr(0,2) );
+    int mm = stoi( d.substr(3,2) );
+    int aa = stoi( d.substr(6,4) );
+
+    initialization(gg,mm,aa);
+}
+*/
+
+std::string Data::formato_breve()
+{
+    return std::to_string(giorno) + "/" +
+           std::to_string(mese) + "/" +
+           std::to_string(anno);
+}
+
---
+++ OOP/costruttore_delegato/data.h
@@ -0,0 +1,54 @@
+/*
+Se non è stato ancora definito questo simbolo non c'è stata occasione per definire data.h
+se un altro progetto dovresse includere questo data.h a quel punto il compilatore lo troverebbe già definito
+e compilato e quindi non lo include.
+
+serve anche a evitare i riferimenti circolari dentro gli header.
+*/
+
+#ifndef DATA_H_INCLUDED
+#define DATA_H_INCLUDED
+#include <string>
+#include <ctime>
+
+class Data
+{
+    //METODI (funzioni)
+
+    //uno dei possibili costruttori
+    //di norma i costruttori sono pubblici per poterli
+    //invocare all'atto della creazione
+    //INTERFACE / INTERFACCIA
+public:
+    Data();
+    Data(int gg); //anno e mese quelli correnti
+    Data(int gg, int mm); // anno quello corrente
+    Data(int gg, int mm, int aa);
+    Data(std::string d); //"24-09-2016"
+
+
+    std::string formato_breve();
+    bool valida(int gg, int mm, int aa);
+    /*
+    Quando un metodo viene inserito all'interno della classe di fatto è trattato come una funzione inline
+    quando verrà richiamata, verrà incorporato nel codice binario una copia di tutta la sua traduzione.
+    Non ci sarà il salto. Ci sarà proprio un copia e incolla nel codice binario.
+    Ogni volta che si richiama una funzione c'è un lavoro di setup aggiuntivo che rappresenta un certo sovraccarico
+    dove l'overhead è superiore alla funzione stessa. In alcuni casi dunque conviene metterlo qui.
+    Da fare con funzioni piccolissime, semplici e che non variano nel tempo.
+    */
+    int getMese() {return mese;}
+    void setMese(int mm) {mese = mm;}
+
+    //metodi statici, invocabili col nome della classe
+    static tm* data_corrente();
+
+    // STATO INTERNO (VARIABILI)
+    //tutte le variabili sono PRIVATE di default
+    // IMPLEMENTAZIONE
+private:
+    int giorno=1, mese=1, anno=1970;
+    static tm* oggi; //non è di un oggetto in particolare, ma di classe quindi condivisa tra tutti gli oggetti
+};
+
+#endif // DATA_H_INCLUDED
